ce035d56d856c3ee440c0cefc455c44b98eef27a
Ju Young Lee <ju1115kr@naver.com>
2018-05-13 14:08:55 +0900

    add server Provider

---
+++ src/providers/server/server.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ServerProvider {
+  serverAddr: string = '';
+
+  constructor(public http: HttpClient
+    , private platform: Platform) {
+    console.log('Hello ServerProvider Provider');
+  }
+
+  get(url) {
+    return new Promise((resolve, reject) => {
+      let serverUrl;
+
+      if (this.platform.is('cordova')) serverUrl = this.serverAddr + url;
+      else serverUrl = "https://meonzzi.newslabfellows.com:9009/api/v1.0/" + url;
+
+      this.http.get(serverUrl)
+        .subscribe((res: any) => {
+          resolve(res);
+        }, (err) => { reject(err) });
+    })
+  }
+
+  post(url, body) {
+    return new Promise((resolve, reject) => {
+      let serverUrl;
+      
+      if (this.platform.is('cordova')) serverUrl = this.serverAddr + url;
+      else serverUrl = "https://meonzzi.newslabfellows.com:9009/api/v1.0/" + url;
+
+      let headers = new HttpHeaders({ 'Content-Type': 'application/json'});
+
+      this.http.post(serverUrl, body, {headers:headers})
+        .subscribe((res: any) => {
+          resolve(res);
+        }, (err) => { reject(err) });
+    })
+
+  }
+}
