ef7408e9fba6bae1f5a64a48446478ff02d390f5
Ju Young Lee <ju1115kr@naver.com>
2018-05-13 14:10:36 +0900

    initialize constructor, add get diarys data in Read Page

---
+++ src/pages/read/read.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { DetailDiaryPage } from '../detail-diary/detail-diary';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'read.html'
+})
+export class ReadPage {
+
+  private diary = [];
+
+  constructor(public navCtrl: NavController,
+    private sqlite: SQLite,
+    private socialSharing: SocialSharing) {
+
+  }
+
+  ionViewDidLoad() {
+    this.getAllDiary();
+  }
+
+  ionViewWillEnter() {
+    this.getAllDiary();
+  }
+
+  getAllDiary() {
+    this.sqlite.create({
+      name: 'emotionDiary.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('select * from diary order by id desc', {})
+        .then(res => {
+          console.log(res);
+          for (let i = 0; i < res.lenght; i++) {
+            this.diary.push(res[i]);
+          }
+        })
+        .catch(e => console.log(e))
+    }).catch(e => console.log(e))
+  }
+
+  getDetailDiary(id) {
+    this.navCtrl.push(DetailDiaryPage);
+  }
+
+}
