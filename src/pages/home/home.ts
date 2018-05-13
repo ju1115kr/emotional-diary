36b0ab269fec17da8bc78d9bd42db08514c8aeb9
Ju Young Lee <ju1115kr@naver.com>
2018-05-13 14:09:47 +0900

    initialize constructor in HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,62 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  recent: {
+    id: number,
+    context: string,
+    slicedcontext: string,
+    filelocate: string,
+    manualValue: number,
+    appHappiness: string,
+    appSorrow: string,
+    appSurprise: string,
+    appAnger: string,
   }
 
+  constructor(public navCtrl: NavController,
+    private sqlite: SQLite,
+    private socialSharing: SocialSharing) {
+  }
+
+  ionViewDidLoad() {
+    this.getData();
+    console.log(this.recent);
+  }
+
+  ionViewWillEnter() {
+    this.getData();
+  }
+
+  getData() {
+    this.sqlite.create({
+      name: 'emotionDiary.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+
+      db.executeSql('create table if not exists diary(id integer primary key, \
+        filelocate text, manualValue int, appHappiness text, appSorrow text, appAnger text, appSurprise text, \
+        context text, datetime datetime', {})
+        .then(res => console.log('Executed SQL: ' + res))
+        .catch(e => console.log(e));
+
+      db.executeSql('select * from diary order by id desc limit 1', {})
+        .then(res => {
+          console.log(res);
+          this.recent.filelocate = res.filelocate;
+          this.recent.context = res.context;
+          this.recent.slicedcontext = res.context.substring(0, 16);
+          this.recent.manualValue = res.manualValue;
+        })
+        .catch(e => console.log(e));
+
+    }).catch(e => console.log(e))
+  }
+
+  shareFacebook() {
+    this.socialSharing.shareViaFacebook(this.recent.context, this.recent.filelocate, '')
+      .then(() => { })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 }
