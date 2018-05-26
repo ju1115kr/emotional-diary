5f8bc3ce60253db583fe1f1dce60d41dff0d0410
Ju Young Lee <ju1115kr@naver.com>
2018-05-26 15:24:14 +0900

    add circle of happiness percentage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,27 +8,22 @@ import { SocialSharing } from '@ionic-native/social-sharing';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   recent: {
-    id: number,
-    context: string,
-    slicedcontext: string,
-    filelocate: string,
-    manualValue: number,
-    appHappiness: string,
-    appSorrow: string,
-    appSurprise: string,
-    appAnger: string,
+    id: number;
+    context: string;
+    slicedcontext: string;
+    filelocate: string;
+    manualValue: number;
   }
+  // emotion: any = { happiness: "", sorrow: "", anger: "", surprise: "" };
+  average: number;
 
   constructor(public navCtrl: NavController,
     private sqlite: SQLite,
-    private socialSharing: SocialSharing) {
-  }
+    private socialSharing: SocialSharing) { }
 
   ionViewDidLoad() {
     this.getData();
-    console.log(this.recent);
   }
 
   ionViewWillEnter() {
@@ -42,27 +37,53 @@ export class HomePage {
     }).then((db: SQLiteObject) => {
 
       db.executeSql('create table if not exists diary(id integer primary key, \
-        filelocate text, manualValue int, appHappiness text, appSorrow text, appAnger text, appSurprise text, \
+        filelocate text, manualValue int, happiness text, sorrow text, anger text, surprise text, \
         context text, datetime datetime', {})
         .then(res => {
           console.log('Executed SQL: ' + res)
         }, (err) => { console.log(err); })
 
-      db.executeSql('select * from diary order by id desc limit 1', {})
+      db.executeSql('select *, avg(happniess) as happniess, avg(sorrow) as sorrow from diary order by id desc limit 1', {})
         .then(res => {
           console.log(res);
           this.recent.filelocate = res.filelocate;
           this.recent.context = res.context;
           this.recent.slicedcontext = res.context.substring(0, 16);
           this.recent.manualValue = res.manualValue;
-        }, (err) => { console.log(err); })
+
+          this.average = parseInt(res.rows.item(0).happniess) / parseInt(res.rows.item(0).sorrow) * 100;
+          this.fillCircle(this.average, document.getElementById('HomeCircle'));
+        }, (err) => {
+          this.average = 0;
+          console.log(err);
+        })
+
     }, (err) => { console.log(err); })
+  }
+
+  fillCircle(value, canvas) {
+    let radius = 12.8;
+    let lineWidth = 3;
+    let context = canvas.getContext('2d');
+
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+    let startAngle = Math.PI * -0.5;
+    let endAngle = startAngle + (value * (Math.PI * 2) / 100);
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.arc(x, y, radius, 0, 0, false);
+    context.beginPath();
+    context.arc(x, y, radius, startAngle, endAngle, false);
+    context.lineWidth = lineWidth;
+    context.strokeStyle = "#B0A9B5";
+
+    context.stroke();
   }
 
   shareFacebook() {
     this.socialSharing.shareViaFacebook(this.recent.context, this.recent.filelocate, '')
       .then(() => {
-
       }, (err) => { console.log(err); })
   }
 }
